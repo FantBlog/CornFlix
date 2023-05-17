269bb999ce2cd43cf9ed3b7457e4430e2bb1a6b7
백승은 <tmdms12@gmail.com>
2023-05-17 16:40:39 +0900

    feat : vue 기본 template 추

---
+++ frontserver/src/views/MainView.vue
@@ -0,0 +1,35 @@
+<template>
+    <div  class="main">
+        <!-- <HeaderLayout /> -->
+        <div v-if="isStateOn">
+        <h1>CORNFLIX</h1>
+        </div>
+        <div v-else>
+        <router-link :to="{ name: 'movies' }">Home</router-link><br>
+        <router-link :to="{ name: 'commu' }">Community</router-link>
+        </div>
+        <img @click="toggleOnOff" alt="home img" src="@/assets/home.png">
+    </div>
+</template>
+
+<script>
+// @ is an alias to /src
+// import HelloWorld from '@/components/HelloWorld.vue'
+
+export default {
+name: 'HomeView',
+components: {
+    // HelloWorld
+},
+data() {
+    return{
+    isStateOn:true
+    }
+},
+methods:{
+    toggleOnOff(){
+    this.isStateOn = !this.isStateOn;
+    }
+}
+}
+</script>
---
+++ frontserver/src/views/NotFoundErr.vue
@@ -0,0 +1,16 @@
+<template>
+  <div>
+    <h1>해당 페이지를 찾을 수 없습니다</h1>
+    <img @click="toggleOnOff" alt="error img" src="@/assets/err.png">
+  </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
