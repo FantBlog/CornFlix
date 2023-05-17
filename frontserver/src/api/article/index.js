f1850be29b49bf1b762c2cc6abce8d85933196fe
백승은 <tmdms12@gmail.com>
2023-05-17 17:21:06 +0900

    feat : api, store/modules 추

---
+++ frontserver/src/api/article/index.js
@@ -0,0 +1,5 @@
+import api from '@/api/base.js';
+
+const fetchArticles = () => api.get('/api/v1/articles/');
+
+export { fetchArticles };
---
+++ frontserver/src/store/modules/article.js
@@ -0,0 +1,24 @@
+import { fetchArticles } from '@/api/article/index.js';
+
+export default {
+  state: {
+    articles: [],
+  },
+  mutations: {
+    GET_ARTICLES(state, articles) {
+      state.articles = articles;
+    },
+  },
+  actions: {
+    getArticles(context) {
+      fetchArticles()
+        .then((res) => {
+          // console.log(res, context)
+          context.commit('GET_ARTICLES', res.data);
+        })
+        .catch((err) => {
+          console.log(err);
+        });
+    },
+  },
+};
