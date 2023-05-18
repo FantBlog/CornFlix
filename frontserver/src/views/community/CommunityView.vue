4b329364182f569900e2a82d6f5370e204513030
FantBlog <tjddnr011012@gmail.com>
2023-05-18 23:04:49 +0900

    feat: community views

---
+++ frontserver/src/views/community/CommunityView.vue
@@ -0,0 +1,33 @@
+<template>
+  <div>
+    <h1>community</h1>
+    <postList></postList>
+  </div>
+</template>
+
+<script>
+import postList from '@/components/community/PostList.vue'
+export default {
+  name: 'CommunityView',
+  components: {
+    postList,
+  },
+  computed:{
+    isLogin() {
+      return this.$store.getters.isLogin // 로그인 여부
+    }
+  },
+  created() {
+    this.getPosts()
+  },
+  methods: {
+    getPosts() {
+      this.$store.dispatch('getPosts')
+    }
+  },
+}
+</script>
+
+<style>
+
+</style>
