b00b37d4f5423c351395d6fbcce7d8031531b35b
FantBlog <tjddnr011012@gmail.com>
2023-05-18 16:49:35 +0900

    feat: 추천영화, 최신영화용 url.py, views,py update

--- backserver/movies/urls.py
+++ backserver/movies/urls.py
@@ -4,6 +4,9 @@ from . import views
 app_name = "movies"
 urlpatterns = [
     path("movies/", views.movie_list, ""),
+    path("movies/recommend/<int:genre_id>/", views.like_recommend_movie_list, ""),
+    path("movies/recent/", views.recent_movie_list, ""),
+    path("movies/<search>/", views.recommend_movie_list, ""),
     path("movies/<int:movie_pk>/", views.movie_detail, ""),
     path("movies/<int:movie_pk>/reviews/", views.review_list, ""),
     path("reviews/<int:review_pk>/", views.review_detail, ""),
--- backserver/movies/views.py
+++ backserver/movies/views.py
@@ -1,12 +1,18 @@
 from django.shortcuts import get_list_or_404, get_object_or_404
+from django.db.models import Case, When
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
+from rest_framework.decorators import permission_classes
+from rest_framework.permissions import IsAuthenticated
 from rest_framework import status
+import datetime
 from .models import Movie, Review
 from .serializers import *
+from .searcher import get_recommendations
 
 
 @api_view(["GET", "POST"])
+@permission_classes([IsAuthenticated])
 def movie_list(request):
     if request.method == "GET":
         movies = get_list_or_404(Movie)
@@ -78,3 +84,41 @@ def review_detail(request, review_pk):
     elif request.method == "DELETE":
         review.delete()
         return Response(status=status.HTTP_204_NO_CONTENT)
+
+
+@api_view(["GET"])
+def recommend_movie_list(request, search):
+    if request.method == "GET":
+        title_list = get_recommendations(search)
+        # 결과 순서를 저장
+        ordering = Case(
+            *[When(title=title, then=pos) for pos, title in enumerate(title_list)]
+        )
+        # 결과 조회 및 저장된 순서로 정렬
+        movie = Movie.objects.filter(title__in=title_list).order_by(ordering)
+
+        serializer = MovieListSerializer(movie, many=True)
+        return Response(serializer.data, status=status.HTTP_200_OK)
+
+
+@api_view(["GET"])
+def like_recommend_movie_list(request, genre_id):
+    if request.method == "GET":
+        genres = [genre_id]
+        movie = Movie.objects.filter(genres__in=genres).order_by("-score")[:20]
+
+        serializer = MovieListSerializer(movie, many=True)
+
+        return Response(serializer.data, status=status.HTTP_200_OK)
+
+
+@api_view(["GET"])
+def recent_movie_list(request):
+    if request.method == "GET":
+        movie = Movie.objects.filter(release_date__lte=datetime.date.today()).order_by(
+            "-release_date"
+        )[:20]
+
+        serializer = MovieListSerializer(movie, many=True)
+
+        return Response(serializer.data, status=status.HTTP_200_OK)
