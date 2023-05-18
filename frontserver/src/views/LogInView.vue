b5820e38cb47a097349a7ec93aae1c2c27c70d81
백승은 <tmdms12@gmail.com>
2023-05-18 09:19:29 +0900

    feat : 회원가입/로그인 뷰 작성

--- frontserver/src/views/LogInView.vue
+++ frontserver/src/views/LogInView.vue
@@ -1,13 +1,45 @@
 <template>
-  <div></div>
+  <div>
+    <h1>로그인</h1>
+    <form @submit="logIn">
+      <label for="username">사용자 이름:</label>
+      <input type="text" id="username" v-model="username" required>
+      <br>
+      <label for="password">비밀번호:</label>
+      <input type="password" id="password" v-model="password" required>
+      <br>
+      <button type="submit">로그인</button>
+    </form>
+  </div>
 </template>
 
 <script>
-export default {
+import axios from 'axios';
 
+export default {
+  data() {
+    return {
+      username: '',
+      password: ''
+    };
+  },
+  methods: {
+    logIn(event) {
+      event.preventDefault();
+      
+      axios.post('/api/login', {
+        username: this.username,
+        password: this.password
+      })
+      .then(response => {
+        // 로그인 성공 시 처리할 로직 작성
+        console.log(response.data);
+      })
+      .catch(error => {
+        // 로그인 실패 시 처리할 로직 작성
+        console.error(error);
+      });
+    }
+  }
 }
 </script>
-
-<style>
-
-</style>
