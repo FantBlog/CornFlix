570125dffa62c698a36279e29a0451ee0e2821c0
FantBlog <tjddnr011012@gmail.com>
2023-05-19 13:21:46 +0900

    feat: vue post api

---
+++ frontserver/src/api/community/post.js
@@ -0,0 +1,29 @@
+import api from '@/api/base.js'
+
+const fetchPosts = () => api.get('/community/posts/')
+
+const fetchCreatePosts = function ({ title, content }) {
+  return api({
+    method: 'post',
+    url: '/community/posts/',
+    data: {
+      title,
+      content,
+    },
+  })
+}
+
+const fetchDetailPost = (post_id) => api.get(`/community/posts/${post_id}/`)
+const fetchDeleteDetailPost = (post_id) => api.delete(`/community/posts/${post_id}/`)
+const fetchPutDetailPost = function ({ post_id, title, content }) {
+  return api({
+    method: 'put',
+    url: `/community/posts/${post_id}/`,
+    data: {
+      title,
+      content,
+    },
+  })
+}
+
+export { fetchPosts, fetchCreatePosts, fetchDetailPost, fetchDeleteDetailPost, fetchPutDetailPost }
