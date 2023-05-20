5329f4eafc87283be4546e5cf1ec136adf05780d
FantBlog <tjddnr011012@gmail.com>
2023-05-20 14:26:43 +0900

    feat: create user profile api

--- backserver/accounts/views.py
+++ backserver/accounts/views.py
@@ -1,3 +1,44 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect, get_object_or_404, get_list_or_404
+from django.contrib.auth import get_user_model
+from rest_framework import status
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+from .serializers import *
+from .models import User
 
-# Create your views here.
+
+@api_view(["GET"])
+def myprofile(request):
+    if request.method == "GET":
+        print(request.user)
+        user = User.objects.get(username=request.user)
+        serializer = UserSerializer(user)
+        return Response(serializer.data, status=status.HTTP_200_OK)
+    
+@api_view(["GET"])
+def profile(request, user_pk):
+    if request.method == "GET":
+        user = User.objects.get(pk=user_pk)
+        serializer = UserSerializer(user)
+        return Response(serializer.data, status=status.HTTP_200_OK)
+
+
+@api_view(["GET", "POST"])
+def follow(request, user_pk):
+
+    if request.method == "GET":
+        posts = User.objects.get(pk=user_pk)
+        serializer = UserSerializer(posts)
+        return Response(serializer.data, status=status.HTTP_200_OK)
+    
+    if request.method == "POST":
+        if request.user.is_authenticated:
+            User = get_user_model()
+            person = User.objects.get(pk=user_pk)
+            if person != request.user:
+                if person.user_followers.filter(pk=request.user.pk).exists():
+                    person.user_followers.remove(request.user)
+                else:
+                    person.user_followers.add(request.user)
+            return redirect('accounts:profile', person.username)
+        return redirect('accounts:login')
