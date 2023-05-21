877ec7e4331e84899ff1226098bd024ffa92ed1e
FantBlog <tjddnr011012@gmail.com>
2023-05-21 17:50:13 +0900

    fix: myprofile 삭제에 따라 api 삭제

--- backserver/accounts/urls.py
+++ backserver/accounts/urls.py
@@ -4,9 +4,7 @@ from . import views
 app_name = 'accounts'
 
 urlpatterns = [
-    path('me/', views.myprofile, name='myprofile'),
-    path('me/review/', views.myprofilereview, name='myprofilereview'),
-    path('profile/<int:user_pk>/', views.profile, name='profile'),
-    path('profile/<int:user_pk>/review/', views.profilereview, name='profilereview'),
-    path('<int:user_pk>/follow/', views.follow, name='follow'),
+    path('profile/<str:user_name>/', views.profile, name='profile'),
+    path('profile/<str:user_name>/review/', views.profilereview, name='profilereview'),
+    path('<str:user_name>/follow/', views.follow, name='follow'),
 ]
--- backserver/accounts/views.py
+++ backserver/accounts/views.py
@@ -6,34 +6,17 @@ from rest_framework.response import Response
 from .serializers import *
 from .models import User
 
-
 @api_view(["GET"])
-def myprofile(request):
+def profile(request, user_name):
     if request.method == "GET":
-        user = User.objects.get(username=request.user)
+        user = User.objects.get(username=user_name)
         serializer = UserSerializer(user)
         return Response(serializer.data, status=status.HTTP_200_OK)
 
 @api_view(["GET"])
-def myprofilereview(request):
+def profilereview(request, user_name):
     if request.method == "GET":
-        user = User.objects.get(username=request.user)
-        reviews = user.review_set.all()
-
-        serializer = UserReviewSerializer(reviews, many=True)
-        return Response(serializer.data, status=status.HTTP_200_OK)
-    
-@api_view(["GET"])
-def profile(request, user_pk):
-    if request.method == "GET":
-        user = User.objects.get(pk=user_pk)
-        serializer = UserSerializer(user)
-        return Response(serializer.data, status=status.HTTP_200_OK)
-
-@api_view(["GET"])
-def profilereview(request, user_pk):
-    if request.method == "GET":
-        user = User.objects.get(pk=user_pk)
+        user = User.objects.get(username=user_name)
         reviews = user.review_set.all()
 
         serializer = UserReviewSerializer(reviews, many=True)
@@ -41,17 +24,17 @@ def profilereview(request, user_pk):
 
 
 @api_view(["GET", "POST"])
-def follow(request, user_pk):
+def follow(request, user_name):
 
     if request.method == "GET":
-        posts = User.objects.get(pk=user_pk)
-        serializer = UserSerializer(posts)
+        User = User.objects.get(username=user_name)
+        serializer = UserSerializer(User)
         return Response(serializer.data, status=status.HTTP_200_OK)
     
     if request.method == "POST":
         if request.user.is_authenticated:
             User = get_user_model()
-            person = User.objects.get(pk=user_pk)
+            person = User.objects.get(username=user_name)
             if person != request.user:
                 if person.user_followers.filter(pk=request.user.pk).exists():
                     person.user_followers.remove(request.user)
