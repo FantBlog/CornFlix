1b40157ddce008dbbdd47e2d89a13bfbe85dc04d
FantBlog <tjddnr011012@gmail.com>
2023-05-23 00:13:06 +0900

    feat: 프로필 이미지 업로드 api

--- backserver/accounts/serializers.py
+++ backserver/accounts/serializers.py
@@ -1,12 +1,10 @@
 from rest_framework import serializers
-from .models import User, UserProfile
+from .models import User
 from movies.models import Movie, Review
 from communitys.models import Post, Comment
 
 
 class MovieSerializer(serializers.ModelSerializer):
-    # user 가 좋아하는 영화 목록에 들어갈 정보
-    # 제목, 링크용 id?, 포스터
     class Meta:
         model = Movie
         fields = "__all__"
@@ -25,25 +23,16 @@ class CommentSerializer(serializers.ModelSerializer):
 
 
 class ReviewSerializer(serializers.ModelSerializer):
+    movie_title = serializers.CharField(source="movie.title")
+    movie_poster_path = serializers.CharField(source="movie.poster_path")
     class Meta:
         model = Review
         exclude = ("updated_at", "user")
 
 
-class UserProfileSerializer(serializers.ModelSerializer):
-    profile_image = serializers.ImageField(
-        max_length=None, allow_empty_file=False, use_url=True
-    )
-
-    class Meta:
-        model = UserProfile
-        fields = "__all__"
-        read_only_fields = ("user",)
-
-
 class UserSerializer(serializers.ModelSerializer):
-    followings_count = serializers.IntegerField(source="followings.count")
-    user_followers_count = serializers.IntegerField(source="user_followers.count")
+    followings_count = serializers.IntegerField(source="followings.count") # 내가 팔로잉
+    user_followers_count = serializers.IntegerField(source="user_followers.count") # 나를 팔로워
     like_movies = MovieSerializer(many=True)
     review_set = ReviewSerializer(many=True)
     post_set = PostSerializer(many=True)
@@ -61,12 +50,26 @@ class UserSerializer(serializers.ModelSerializer):
             "groups",
             "user_permissions",
         ]
+        read_only_fields = (
+            "followings_count",
+            "user_followers_count",
+            "like_movies",
+            "review_set",
+            "post_set",
+            "comment_set",
+        )
 
-
-class UserReviewSerializer(serializers.ModelSerializer):
-    movie_title = serializers.CharField(source="movie.title")
-    movie_poster_path = serializers.CharField(source="movie.poster_path")
-
+class UserListSerializer(serializers.ModelSerializer):
     class Meta:
-        model = Review
-        fields = "__all__"
+        model = User
+        exclude = [
+            "username",
+            "last_login",
+            "is_superuser",
+            "is_staff",
+            "password",
+            "is_active",
+            "date_joined",
+            "groups",
+            "user_permissions",
+        ]
--- backserver/accounts/views.py
+++ backserver/accounts/views.py
@@ -1,60 +1,38 @@
-from django.shortcuts import render, redirect, get_object_or_404, get_list_or_404
+from django.shortcuts import get_object_or_404
 from django.contrib.auth import get_user_model
 from rest_framework import status
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
 from .serializers import *
-from .models import User, UserProfile
-
-
-@api_view(["GET"])
-def profile(request, user_name):
-    if request.method == "GET":
-        user = User.objects.get(username=user_name)
-        serializer = UserSerializer(user)
-        return Response(serializer.data, status=status.HTTP_200_OK)
+from .models import User
 
 
 @api_view(["GET", "PUT"])
-def userprofile(request, user_name):
-    user = User.objects.get(username=user_name)
-    userprofile = get_object_or_404(UserProfile, user=user.pk)
+def profile(request, user_name):
+    user = get_object_or_404(User, username=user_name)
 
     if request.method == "GET":
-        serializer = UserProfileSerializer(userprofile)
+        serializer = UserSerializer(user)
         return Response(serializer.data, status=status.HTTP_200_OK)
 
     elif request.method == "PUT":
-        serializer = UserProfileSerializer(
-            instance=userprofile, data=request.data, files=request.FILES
-        )
-        print(request.FILES)
+        serializer = UserListSerializer(instance=user, data=request.data)
         if serializer.is_valid(raise_exception=True):
-            serializer.save(user=user)
+            serializer.save(username=user_name)
+            serializer = UserSerializer(user)
             return Response(serializer.data, status=status.HTTP_200_OK)
-
-
-@api_view(["GET"])
-def profilereview(request, user_name):
-    if request.method == "GET":
-        user = User.objects.get(username=user_name)
-        reviews = user.review_set.all()
-
-        serializer = UserReviewSerializer(reviews, many=True)
-        return Response(serializer.data, status=status.HTTP_200_OK)
-
 
 @api_view(["GET", "POST"])
 def follow(request, user_name):
     if request.method == "GET":
-        User = User.objects.get(username=user_name)
+        user = get_object_or_404(User,username=user_name)
         serializer = UserSerializer(User)
         return Response(serializer.data, status=status.HTTP_200_OK)
 
     if request.method == "POST":
         if request.user.is_authenticated:
             User = get_user_model()
-            person = User.objects.get(username=user_name)
+            person = get_object_or_404(User,username=user_name)
             if person != request.user:
                 if person.user_followers.filter(pk=request.user.pk).exists():
                     person.user_followers.remove(request.user)
