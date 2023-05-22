8eb3ef6fd53a19357234c64b8755ae2b219488dc
백승은 <tmdms12@gmail.com>
2023-05-22 16:39:45 +0900

    refactor : font 변경

--- frontserver/src/App.vue
+++ frontserver/src/App.vue
@@ -27,5 +27,26 @@ export default {
   color: rgb(255, 255, 255);
   /* color: rgb(230, 230, 230); */
   background-color: rgb(15, 15, 15);
+  /* font-family: 'Noto Sans KR', sans-serif; */
+  font-family: 'Gowun Batang', serif;
 }
+/* @font-face {
+    font-family: 'SUITE-Regular';
+    src: url('https://cdn.jsdelivr.net/gh/projectnoonnu/noonfonts_2304-2@1.0/SUITE-Regular.woff2') format('woff2');
+    font-weight: 400;
+    font-style: normal;
+} */
+/* @font-face {
+    font-family: 'NanumSquareNeo-Variable';
+    src: url('https://cdn.jsdelivr.net/gh/projectnoonnu/noonfonts_11-01@1.0/NanumSquareNeo-Variable.woff2') format('woff2');
+    font-weight: normal;
+    font-style: normal;
+} */
+/* @import url(//fonts.googleapis.com/earlyaccess/nanummyeongjo.css);
+.nanummyeongjo * {
+ font-family: 'Nanum Myeongjo', serif;
+} */
+
+@import url('https://fonts.googleapis.com/css2?family=Noto+Sans+KR&display=swap');
+@import url('https://fonts.googleapis.com/css2?family=Gowun+Batang&display=swap');
 </style>
