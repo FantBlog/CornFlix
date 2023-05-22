033c39051ae2453770bacf818a69dc139432715d
FantBlog <tjddnr011012@gmail.com>
2023-05-22 11:07:07 +0900

    feat: 본인 프로필일때 follow 검사 하지 않게 변경

--- frontserver/src/store/modules/profile.js
+++ frontserver/src/store/modules/profile.js
@@ -7,17 +7,20 @@ export default {
   },
   mutations: {
     GET_PROFILE(state, profile) {
-      const followers = profile.user_followers
-      state.isFollowing = false
-      followers.forEach(user => {
-        if (user.username == this.state.user.username) {
-          state.isFollowing = true
-        }
-      })
-      state.profile = profile;
+      if (profile.username !== this.state.user.username) {
+        const followers = profile.user_followers
+        state.isFollowing = false
+        followers.forEach(user => {
+          if (user.username == this.state.user.username) {
+            state.isFollowing = true
+          }
+        })
+      }
+      else state.isFollowing = false
+      state.profile = profile
     },
     GET_REVIEW(state, review) {
-      state.review = review;
+      state.review = review
     }
   },
   actions: {
@@ -26,33 +29,33 @@ export default {
       fetchProfile({ user_name })
         .then((res) => {
           console.log(res.data)
-          context.commit('GET_PROFILE', res.data);
+          context.commit('GET_PROFILE', res.data)
         })
         .catch((err) => {
-          console.log(err);
-        });
+          console.log(err)
+        })
     },
     getReview(context, payload) {
       const user_name = payload.user_name
       fetchReview({ user_name })
         .then((res) => {
           // console.log(res, context)
-          context.commit('GET_REVIEW', res.data);
+          context.commit('GET_REVIEW', res.data)
         })
         .catch((err) => {
-          console.log(err);
-        });
+          console.log(err)
+        })
     },
     postFollow(context, payload) {
       const user_name = payload.user_name
       fetchFollow({ user_name })
         .then((res) => {
           // console.log(res.data)
-          context.commit('GET_PROFILE', res.data);
+          context.commit('GET_PROFILE', res.data)
         })
         .catch((err) => {
-          console.log(err);
-        });
+          console.log(err)
+        })
     },
   },
-};
+}
