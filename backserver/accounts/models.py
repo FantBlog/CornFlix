0a9f975cef31f63d98294a8c990e7118a77264c0
FantBlog <tjddnr011012@gmail.com>
2023-05-23 00:12:45 +0900

    del: user profile api 병합

--- backserver/accounts/models.py
+++ backserver/accounts/models.py
@@ -4,14 +4,7 @@ from django.contrib.auth.models import AbstractUser
 
 class User(AbstractUser):
     followings = models.ManyToManyField(
-        "self", symmetrical=False, related_name="user_followers"
+        "self", symmetrical=False, related_name="user_followers", blank=True
     )
-
-
-class UserProfile(models.Model):
-    user = models.OneToOneField(User, on_delete=models.CASCADE)
     content = models.TextField(blank=True)
     profile_image = models.ImageField(upload_to="profile_images/", blank=True)
-
-    def __str__(self):
-        return self.user.username
--- backserver/accounts/urls.py
+++ backserver/accounts/urls.py
@@ -5,7 +5,5 @@ app_name = "accounts"
 
 urlpatterns = [
     path("profile/<str:user_name>/", views.profile, name="profile"),
-    path("userprofile/<str:user_name>/", views.userprofile, name="userprofile"),
-    path("profile/<str:user_name>/review/", views.profilereview, name="profilereview"),
     path("<str:user_name>/follow/", views.follow, name="follow"),
 ]
