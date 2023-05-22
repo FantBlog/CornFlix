3ad3fcc626877cfae32dd2ae86de452884ad8ef5
백승은 <tmdms12@gmail.com>
2023-05-22 18:04:52 +0900

    feat : 중간저장

--- frontserver/src/components/movie/RecentMovieListItem.vue
+++ frontserver/src/components/movie/RecentMovieListItem.vue
@@ -2,10 +2,10 @@
   
   <div class="col">
     <div class="movie-list-item card" @click="goToDetailPage">
-      <img :src=movie?.poster_path class="card-img-top" alt="...">
-      <div class="card-body">
-        <h5 class="card-title">{{ movie.title }}</h5>
-      </div>
+      <img :src=movie.poster_path class="card-img-top" alt="...">
+      <!-- <div class="card-body"> -->
+        <!-- <h5 class="card-title">{{ movie.title }}</h5> -->
+      <!-- </div> -->
     </div>
   </div>
   <!-- <div class="movie-list-item card" @click="goToDetailPage">
@@ -34,8 +34,8 @@ export default {
 .movie-list-item {
   width: 200px;
   margin: 10px;
-  border: 1px solid #ccc;
-  border-radius: 5px;
+  /* border: 1px solid #ccc; */
+  border: none;
   text-align: center;
   cursor: pointer;
   transition: all 0.3s;
@@ -43,6 +43,7 @@ export default {
 
 .movie-list-item:hover {
   transform: scale(1.1);
+  z-index: 1;
   box-shadow: 0 0 5px rgba(0, 0, 0, 0.3);
 }
 
@@ -51,14 +52,13 @@ export default {
   object-fit: cover;
 }
 
-.card-body {
+/* .card-body {
   padding: 10px;
-}
-
-.card-title {
+} */
+/* .card-title {
   font-size: 16px;
   font-weight: bold;
   margin-bottom: 0;
   color: black;
-}
+} */
 </style>
