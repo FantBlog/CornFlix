d26a51f16fe5fe9a99d955b54f56bd989c2b759f
FantBlog <tjddnr011012@gmail.com>
2023-05-24 11:39:09 +0900

    refactor: api urls, views 순서 정렬

--- final-pjt-back/final-pjt-back/urls.py
+++ final-pjt-back/final-pjt-back/urls.py
@@ -22,7 +22,7 @@ from django.conf.urls.static import static
 
 urlpatterns = [
     path("admin/", admin.site.urls),
-    path("api/v1/", include("movies.urls")),
+    path("movies/", include("movies.urls")),
     path("community/", include("communitys.urls")),
     path("user/", include("accounts.urls")),
     path("accounts/", include("dj_rest_auth.urls")),
