2c79822b9d58828bd608982fe89d494dd8b173fc
FantBlog <tjddnr011012@gmail.com>
2023-05-24 11:39:32 +0900

    refactor: urls 변경에 따른 axios 변경

--- final-pjt-front/src/api/movie/index.js
+++ final-pjt-front/src/api/movie/index.js
@@ -3,42 +3,42 @@ import api from '@/api/base.js'
 const fetchRecentMovies = function ({ page }) {
   return api({
     method: 'get',
-    url: `/api/v1/movies/recent/${page}/`
+    url: `/movies/recent/${page}/`
   })
 }
 
 const fetchRecommendMovies = function ({ page }) {
   return api({
     method: 'get',
-    url: `/api/v1/movies/recommend/${page}/`
+    url: `/movies/recommend/${page}/`
   })
 }
 
 const fetchGenreMovies = function ({ genre_id, page }) {
   return api({
     method: 'get',
-    url: `/api/v1/movies/genre/${genre_id}/${page}/`
+    url: `/movies/genre/${genre_id}/${page}/`
   })
 }
 
 const fetchTypeMovies = function ({ type, page }) {
   return api({
     method: 'get',
-    url: `/api/v1/movies/${type}/${page}/`
+    url: `/movies/${type}/${page}/`
   })
 }
 
 const fetchDetailMovie = function ({ movie_id }) {
   return api({
     method: 'get',
-    url: `/api/v1/movies/${movie_id}/`
+    url: `/movies/${movie_id}/`
   })
 }
 
 const fetchRelateMovies = function ({ movie_id }) {
   return api({
     method: 'get',
-    url: `/api/v1/movies/relate/${movie_id}/`
+    url: `/movies/relate/${movie_id}/`
   })
 }
 
