a9b30b40e8a93587f3cbb96bb036618d1ddca3f5
백승은 <tmdms12@gmail.com>
2023-05-24 17:31:09 +0900

    refactor : 사용하지 않는 폰트 정리

--- final-pjt-front/src/App.vue
+++ final-pjt-front/src/App.vue
@@ -47,23 +47,6 @@ export default {
   box-sizing: border-box;
 }
 
-/* @font-face {
-    font-family: 'SUITE-Regular';
-    src: url('https://cdn.jsdelivr.net/gh/projectnoonnu/noonfonts_2304-2@1.0/SUITE-Regular.woff2') format('woff2');
-    font-weight: 400;
-    font-style: normal;
-} */
-/* @font-face {
-    font-family: 'NanumSquareNeo-Variable';
-    src: url('https://cdn.jsdelivr.net/gh/projectnoonnu/noonfonts_11-01@1.0/NanumSquareNeo-Variable.woff2') format('woff2');
-    font-weight: normal;
-    font-style: normal;
-} */
-/* @import url(//fonts.googleapis.com/earlyaccess/nanummyeongjo.css);
-.nanummyeongjo * {
- font-family: 'Nanum Myeongjo', serif;
-} */
-
 @import url('https://fonts.googleapis.com/css2?family=Noto+Sans+KR&display=swap');
 @import url('https://fonts.googleapis.com/css2?family=Gowun+Batang&display=swap');
 </style>
